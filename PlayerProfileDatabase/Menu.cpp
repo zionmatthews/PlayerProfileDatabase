d42af0b3983644dd33b87dca9f4135d671364c34
zion matthews <zman5682@gmail.com>
2020-02-03 17:54:44 -0600

    Need to setup up the player profile

--- PlayerProfileDatabase/Menu.cpp
+++ PlayerProfileDatabase/Menu.cpp
@@ -11,11 +11,12 @@ void Menu::start()
 		int choice;
 		cout << "\n\n=============== PLAYER MODIFY MENU V3 ===================";
 		cout << "\n1: Create a player profile.";
-		cout << "\n2: Modify a player profile.";
-		cout << "\n3: Save a player profile.";
-		cout << "\n4: load a player profile.";
-		cout << "\n5: Search for a player profile.";
-		cout << "\n6: Exit from program";
+		cout << "\n2: Remove a player profile.";
+		cout << "\n3: Modify a player profile.";
+		cout << "\n4: Save a player profile.";
+		cout << "\n5: load a player profile.";
+		cout << "\n6: Search for a player profile.";
+		cout << "\n7: Exit from program.";
 	
 		cout << "\n\nEnter your choice...!\n";
 		cin >> choice;
@@ -23,7 +24,10 @@ void Menu::start()
 
 		switch (choice) {
 
+		case 7: exit(0); //closes the program		
 
+		default: cout << "Wrong Input, please try again...\n";
+			break;
 		}
 	}
 }
